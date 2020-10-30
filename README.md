5ec57dda6595ae168e8cd59314c351122bf9d2f0
MeggieJoyce <liaoyuanjing@163.com>
2020-10-31 03:00:01 +0800

    13 修改功能完成

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Node+Mongodb
 cnpm install moment -s   时间戳下载
 
 hidden
+
+api:
+sort()  limit()  skip()  insertOne
+updateOne
+
+bug:
+修改文章的时候，有时候会bug，点击修改再重新发布，就是另外发布了一篇新的文章，而不是覆盖原文章。
+正在测试修改这个Bug中
--- routes/article.js
+++ routes/article.js
@@ -6,27 +6,44 @@ var model = require('../model');
 // 新增、编辑
 router.post('/add', function(req, res, next) {
       var id = parseInt(req.body.id)
-      if(id){       //修改
-  
-      }else{
-                //发布
-      }
-var data = {
-    title: req.body.title,
-     content:req.body.content,
-     id:Date.now(),
-     username:req.session.username
+      if(id){                //编辑
+        var page = req.body.page
+        var title = req.body.title
+        var content = req.body.content
+        model.connect(function(db){
+          db.collection('articles').updateOne({id:id},{$set:{
+            title: title,
+            content: content
+          }},function(err,ret){
+            if(err){
+              console.log('修改帖子失败了！',err)
+            }else{
+              console.log('修改帖子成功')
+              //修改成功 跳转回当前页
+              res.redirect('/?page='+page)
+            }
+          })
+        })
+
+      }else{                 //新增
+      var data = {
+        title: req.body.title,
+          content:req.body.content,
+          id:Date.now(),
+          username:req.session.username
+    }
+    model.connect(function(db) {
+        db.collection('articles').insertOne(data, function(err, ret) {
+          if(err) {
+            console.log('文件发布失败', err)
+            res.redirect('/write')
+          } else {
+            res.redirect('/')
+            }
+          })
+        })
 }
-model.connect(function(db) {
-    db.collection('articles').insertOne(data, function(err, ret) {
-      if(err) {
-        console.log('文件发布失败', err)
-        res.redirect('/write')
-      } else {
-        res.redirect('/')
-        }
-     })
-    })
+
 })
 
 
